21e06568f0954bb912169532adaca748973a43b3
j-hang <jo.hh@cizion.com>
2015-12-16 15:29:58 +0900

    modify

--- hiyo.js
+++ hiyo.js
@@ -2,21 +2,3 @@
 
 안녕하세요
 
-Q2
-누구신가요???
-
-케누나 남친분의 성함은 내자신 입니다.
-
-<<<<<<< HEAD
-누군가가 말했다. 수육드시나요? 아니오 이건 삼겹살 삶은 겁니다.
-!!!
-
-:-)
-
-:-0
-=======
-음하하하하
-alsdjflkadsjlfkjad
-falsdjglkajsdgf
-asdjlfkjasdlfjlaksdjf
->>>>>>> johh
